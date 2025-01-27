74e02012d1a955d2eddb267321ff0c1e5dc5ca5e
Savita Ravindra Shejwal <shejwalsavi2002@gmail.com>
2025-01-27 14:53:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+  window.onload = function() {
+     // Hide the loader and show the content
+      document.getElementById('loader').style.display = 'none'; 
+      document.getElementById('home').style.display = 'block';
+  };
+  
